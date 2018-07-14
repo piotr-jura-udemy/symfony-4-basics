5dbca3d17fe6c65c1b52b956f2c6953112f9dd66
Piotr Jura <piotr.jura.udemy@gmail.com>
2018-07-14 12:26:53 +0200

    Remove Request dependency

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -4,7 +4,6 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
