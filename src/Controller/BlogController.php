8c946e2da064e0b7226dd7e555c3e0d5193def36
Piotr Jura <piotr.jura.udemy@gmail.com>
2018-07-14 12:03:54 +0200

    Initial commit

---
+++ src/Controller/BlogController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/blog")
+ */
+class BlogController extends AbstractController
+{
+    private const POSTS = [
+        [
+            'id' => 1,
+            'slug' => 'hello-world',
+            'title' => 'Hello world!',
+        ],
+        [
+            'id' => 2,
+            'slug' => 'another-post',
+            'title' => 'This is another post!',
+        ],
+        [
+            'id' => 3,
+            'slug' => 'last-example',
+            'title' => 'This is the last example',
+        ],
+    ];
+
+    /**
+     * @Route("/{page}", name="blog_list", defaults={"page": 5})
+     */
+    public function list($page = 1)
+    {
+        return new JsonResponse(
+            [
+                'page' => $page,
+                'data' => self::POSTS
+            ]
+        );
+    }
+
+    /**
+     * @Route("/post/{id}", name="blog_by_id", requirements={"id"="\d+"})
+     */
+    public function post($id)
+    {
+        return new JsonResponse(
+            self::POSTS[array_search($id, array_column(self::POSTS, 'id'))]
+        );
+    }
+
+    /**
+     * @Route("/post/{slug}", name="blog_by_slug")
+     */
+    public function postBySlug($slug)
+    {
+        return new JsonResponse(
+            self::POSTS[array_search($slug, array_column(self::POSTS, 'slug'))]
+        );
+    }
+}
---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/")
+ */
+class DefaultController extends AbstractController
+{
+    /**
+     * @Route("/", name="default_index")
+     */
+    public function index()
+    {
+        return new JsonResponse([
+            'action' => 'index',
+            'time' => time()
+        ]);
+    }
+}
