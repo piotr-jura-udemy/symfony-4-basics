c82c8c401c0cfcbfc65582b5ac45814b611f4e4b
Piotr Jura <piotr.jura.udemy@gmail.com>
2018-07-20 19:06:09 +0200

    Api Platform installation

--- src/Entity/BlogPost.php
+++ src/Entity/BlogPost.php
@@ -2,10 +2,12 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Core\Annotation\ApiResource;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\BlogPostRepository")
+ * @ApiResource()
  */
 class BlogPost
 {
