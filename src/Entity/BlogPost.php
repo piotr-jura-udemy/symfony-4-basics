30ca56fcd26bf80ec8748797f2ba8248eb276866
Piotr Jura <piotr.jura.udemy@gmail.com>
2018-07-29 16:51:38 +0200

    PUT action for modifying BlogPost, and restriction that only owner can modify his own blog posts

--- src/Entity/BlogPost.php
+++ src/Entity/BlogPost.php
@@ -11,7 +11,12 @@ use Symfony\Component\Validator\Constraints as Assert;
 /**
  * @ORM\Entity(repositoryClass="App\Repository\BlogPostRepository")
  * @ApiResource(
- *     itemOperations={"get"},
+ *     itemOperations={
+ *         "get",
+ *         "put"={
+ *             "access_control"="is_granted('IS_AUTHENTICATED_FULLY') and object.getAuthor() == user"
+ *         }
+ *     },
  *     collectionOperations={
  *         "get",
  *         "post"={
