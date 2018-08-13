958997616c1d4318991f8522e95ecedfa904ca00
Piotr Jura <piotr.jura.udemy@gmail.com>
2018-08-13 09:14:14 +0200

    Install and configure VichUploader

--- config/bundles.php
+++ config/bundles.php
@@ -15,4 +15,5 @@ return [
     ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
+    Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
 ];
