5d9344de18dcbe0864fac0eeed9e95dcd365b573
Piotr Jura <piotr.jura.udemy@gmail.com>
2018-08-15 14:29:57 +0200

    Linking Images with BlogPost

--- src/Entity/BlogPost.php
+++ src/Entity/BlogPost.php
@@ -88,9 +88,18 @@ class BlogPost implements AuthoredEntityInterface, PublishedDateEntityInterface
      */
     private $comments;
 
+    /**
+     * @ORM\ManyToMany(targetEntity="App\Entity\Image")
+     * @ORM\JoinTable()
+     * @ApiSubresource()
+     * @Groups({"post"})
+     */
+    private $images;
+
     public function __construct()
     {
         $this->comments = new ArrayCollection();
+        $this->images = new ArrayCollection();
     }
 
     public function getComments(): Collection
@@ -165,5 +174,20 @@ class BlogPost implements AuthoredEntityInterface, PublishedDateEntityInterface
         $this->author = $author;
 
         return $this;
+    }
+
+    public function getImages(): Collection
+    {
+        return $this->images;
+    }
+
+    public function addImage(Image $image)
+    {
+        $this->images->add($image);
+    }
+
+    public function removeImage(Image $image)
+    {
+        $this->images->removeElement($image);
     }
 }
