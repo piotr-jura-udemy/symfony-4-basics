6a04532683b328e90e07a3138af612f9f4dc5d03
Piotr Jura <piotr.jura.udemy@gmail.com>
2018-08-15 17:21:06 +0200

    BlogPost SearchFilter

--- src/Entity/BlogPost.php
+++ src/Entity/BlogPost.php
@@ -2,6 +2,7 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Core\Annotation\ApiFilter;
 use ApiPlatform\Core\Annotation\ApiResource;
 use ApiPlatform\Core\Annotation\ApiSubresource;
 use Doctrine\Common\Collections\ArrayCollection;
@@ -10,9 +11,18 @@ use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Serializer\Annotation\Groups;
 use Symfony\Component\Validator\Constraints as Assert;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\SearchFilter;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\BlogPostRepository")
+ * @ApiFilter(
+ *     SearchFilter::class,
+ *     properties={
+ *         "id": "exact",
+ *         "title": "partial",
+ *         "content": "partial"
+ *     }
+ * )
  * @ApiResource(
  *     attributes={"order"={"published": "DESC"}},
  *     itemOperations={
