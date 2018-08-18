96e82834d7a5f91eff4bfac8d8f04f51cf4e2fc7
Piotr Jura <piotr.jura.udemy@gmail.com>
2018-08-18 17:46:41 +0200

    Installing monolog and logging

--- config/bundles.php
+++ config/bundles.php
@@ -16,4 +16,5 @@ return [
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
 ];
--- src/Security/UserConfirmationService.php
+++ src/Security/UserConfirmationService.php
@@ -5,6 +5,7 @@ namespace App\Security;
 use App\Exception\InvalidConfirmationTokenException;
 use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Psr\Log\LoggerInterface;
 
 class UserConfirmationService
 {
@@ -16,29 +17,40 @@ class UserConfirmationService
      * @var EntityManagerInterface
      */
     private $entityManager;
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
 
     public function __construct(
         UserRepository $userRepository,
-        EntityManagerInterface $entityManager
+        EntityManagerInterface $entityManager,
+        LoggerInterface $logger
     )
     {
         $this->userRepository = $userRepository;
         $this->entityManager = $entityManager;
+        $this->logger = $logger;
     }
 
     public function confirmUser(string $confirmationToken)
     {
+        $this->logger->debug('Fetching user by confirmation token');
+
         $user = $this->userRepository->findOneBy(
             ['confirmationToken' => $confirmationToken]
         );
 
         // User was NOT found by confirmation token
         if (!$user) {
+            $this->logger->debug('User by confirmation token not found');
             throw new InvalidConfirmationTokenException();
         }
 
         $user->setEnabled(true);
         $user->setConfirmationToken(null);
         $this->entityManager->flush();
+
+        $this->logger->debug('Confirmed user by confirmation token');
     }
 }
