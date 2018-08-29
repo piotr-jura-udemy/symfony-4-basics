3d2b6e701aae920e4510eeacad8808b98f8ae196
Piotr Jura <piotr.jura.udemy@gmail.com>
2018-08-29 17:17:00 +0200

    First real unit test

---
+++ tests/EventSubscriber/AuthoredEntitySubscriberTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Tests\EventSubscriber;
+
+use ApiPlatform\Core\EventListener\EventPriorities;
+use App\Entity\User;
+use App\EventSubscriber\AuthoredEntitySubscriber;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\HttpKernel\Event\GetResponseForControllerResultEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+
+class AuthoredEntitySubscriberTest extends TestCase
+{
+    public function testConfiguration()
+    {
+        $result = AuthoredEntitySubscriber::getSubscribedEvents();
+
+        $this->assertArrayHasKey(KernelEvents::VIEW, $result);
+        $this->assertEquals(
+            ['getAuthenticatedUser', EventPriorities::PRE_WRITE],
+            $result[KernelEvents::VIEW]
+        );
+    }
+}
