583b920b478c572ab0ec1cca8ab4192e1c37ab60
Piotr Jura <piotr.jura.udemy@gmail.com>
2018-09-07 17:28:11 +0200

    Authenticated testing, JSON matching

--- features/blog_posts.feature
+++ features/blog_posts.feature
@@ -1,6 +1,7 @@
 Feature: Manage blog posts
   @createSchema
   Scenario: Create a blog post
+    Given I am authenticated as "admin"
     When I add "Content-Type" header equal to "application/ld+json"
     And I add "Accept" header equal to "application/ld+json"
     And I send a "POST" request to "/api/blog_posts" with body:
@@ -13,3 +14,21 @@ Feature: Manage blog posts
     """
     Then the response status code should be 201
     And the response should be in JSON
+    And the JSON matches expected template:
+    """
+    {
+      "@context": "/api/contexts/BlogPost",
+      "@id": "@string@",
+      "@type": "BlogPost",
+      "comments": [],
+      "id": @integer@,
+      "title": "Hello a title",
+      "content": "The content is suppose to be at least 20 characters",
+      "slug": "a-new-slug",
+      "published": "@string@.isDateTime()",
+      "author": "/api/users/1",
+      "images": []
+    }
+    """
+
+
--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -1,7 +1,20 @@
 <?php
 
+use Behat\Gherkin\Node\PyStringNode;
+
 class FeatureContext extends \Behatch\Context\RestContext
 {
+    const USERS = [
+        'admin' => 'secret123#'
+    ];
+    const AUTH_URL = '/api/login_check';
+    const AUTH_JSON = '
+        {
+            "username": "%s",
+            "password": "%s"
+        }
+    ';
+
     /**
      * @var \App\DataFixtures\AppFixtures
      */
@@ -26,6 +39,44 @@ class FeatureContext extends \Behatch\Context\RestContext
         $this->matcher =
             (new \Coduo\PHPMatcher\Factory\SimpleFactory())->createMatcher();
         $this->em = $em;
+    }
+
+    /**
+     * @Given I am authenticated as :user
+     */
+    public function iAmAuthenticatedAs($user)
+    {
+        $this->request->setHttpHeader('Content-Type', 'application/ld+json');
+        $this->request->send(
+            'POST',
+            $this->locatePath(self::AUTH_URL),
+            [],
+            [],
+            sprintf(self::AUTH_JSON, $user, self::USERS[$user])
+        );
+
+        $json = json_decode($this->request->getContent(), true);
+        // Make sure the token was returned
+        $this->assertTrue(isset($json['token']));
+
+        $token = $json['token'];
+
+        $this->request->setHttpHeader(
+            'Authorization',
+            'Bearer '.$token
+        );
+    }
+
+    /**
+     * @Then the JSON matches expected template:
+     */
+    public function theJsonMatchesExpectedTemplate(PyStringNode $json)
+    {
+        $actual = $this->request->getContent();
+        var_dump($actual);
+        $this->assertTrue(
+            $this->matcher->match($actual, $json->getRaw())
+        );
     }
 
     /**
