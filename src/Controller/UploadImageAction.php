4e44de90d5dd316ae8fddfc718f12c57a114e02c
ceder <ceder16@gmail.com>
2021-08-10 22:01:07 +0200

    Restore UploadImageAction.php

---
+++ src/Controller/UploadImageAction.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Controller;
+
+use ApiPlatform\Core\Validator\Exception\ValidationException;
+use ApiPlatform\Core\Validator\ValidatorInterface;
+use App\Entity\Image;
+use App\Form\ImageType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Form\FormFactoryInterface;
+use Symfony\Component\HttpFoundation\Request;
+
+class UploadImageAction
+{
+    /**
+     * @var FormFactoryInterface
+     */
+    private $formFactory;
+    /**
+     * @var EntityManagerInterface
+     */
+    private $entityManager;
+    /**
+     * @var ValidatorInterface
+     */
+    private $validator;
+
+    public function __construct(
+        FormFactoryInterface $formFactory,
+        EntityManagerInterface $entityManager,
+        ValidatorInterface $validator
+    )
+    {
+        $this->formFactory = $formFactory;
+        $this->entityManager = $entityManager;
+        $this->validator = $validator;
+    }
+
+    public function __invoke(Request $request)
+    {
+        // Create a new Image instance
+        $image = new Image();
+        // Validate the form
+        $form = $this->formFactory->create(ImageType::class, $image);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Persist the new Image entity
+            $this->entityManager->persist($image);
+            $this->entityManager->flush();
+
+            $image->setFile(null);
+
+            return $image;
+        }
+
+        // Uploading done for us in background by VichUploader...
+
+        // Throw an validation exception, that means something went wrong during
+        // form validation
+        throw new ValidationException(
+            $this->validator->validate($image)
+        );
+    }
+}
